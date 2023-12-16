3ff88f3cb82ce56a74b8b3bb2591928e81180043
Renan <renansalmazio@gmail.com>
2023-12-16 20:15:43 -0300

    arquivos do backend da aplicação

---
+++ backend/index.js
@@ -0,0 +1,58 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import {connection} from './database/database.js'
+const App = express()
+const PORT = 3000
+
+
+// informa ao sistema que ira utilizar entradas do tipo json
+App.use(express.json());
+App.use(bodyParser.json())
+App.use(bodyParser.urlencoded({extended: true}))
+
+
+//rota principal da aplicação 
+App.get("/",(req,resp)=>{
+   resp.send({message: "por aqui tudo ok"})
+})
+
+//pega todos os produtos do banco 
+App.get('/produtos',(req,resp)=> {
+    const query = "SELECT * FROM produtos"
+    
+
+    connection.query(query,(err, data) => {
+        if(err) return  resp.json(error)
+         
+        return resp.json(data)
+    })
+
+})
+
+// inseri produtos no banco 
+App.post("/produtos", (req, resp) => {
+    const data  = req.body
+    const query = "INSERT INTO produtos (codigoProduto,nome,descrisao,preco) values (?)"
+    const values = [
+        data.codigoProduto,
+        data.nome,
+        data.descrisao,
+        data.preco
+    ]
+    
+    connection.query(query,[values],(err,data) => {
+        if(err) return resp.send(err)
+
+        return resp.json(data)
+    })
+} )
+
+
+
+
+
+
+//sobe o sitema na porta 3000
+App.listen(PORT, () => {
+    console.log(`A aplicação esta rodando na porta ${PORT}`);
+})
