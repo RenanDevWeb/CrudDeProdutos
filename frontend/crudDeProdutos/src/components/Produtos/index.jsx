8f7df52ce88e77e98053bd709bb9fba60dc230ad
Renan <renansalmazio@gmail.com>
2023-12-19 19:48:19 -0300

    arquivos apenas para consulta

--- frontend/crudDeProdutos/src/components/Produtos/index.jsx
+++ frontend/crudDeProdutos/src/components/Produtos/index.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import axios from 'axios';
 import './index.css'
 
 
 export default function Produtos() {
-  return (
-    <div className='produtos'>
-    Pagina de produtos 
-    </div>
-  )
-}
+ const [produtos,setProdutos] = useState([])
+
+   useState(() => {
+     getProdutos()
+   },[])
+
+
+  async function getProdutos(){
+    axios.get('http://localhost:3000/produtos')
+    .then(function (response) {
+      // aqui acessamos o corpo da resposta:
+      setProdutos(response.data)
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // aqui temos acesso ao erro, quando alguma coisa inesperada acontece:
+      console.log(error);
+    })
+    }
+
+
+
+    return (
+  
+ 
+      <div className='produtos'>
+      <table>
+        <thead>
+          <tr>
+            <th>Código do Produto</th>
+            <th>Nome</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+          </tr>
+        </thead>
+        <tbody>
+          {produtos.map(product => (
+            <tr key={product.codigoProduto}>
+              <td>{product.codigoProduto}</td>
+              <td>{product.nome}</td>
+              <td>{product.descrisao}</td>
+              <td>R$ {product.preco.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      </div>
+    
+    )
+  }
+  
+
+
+ 
